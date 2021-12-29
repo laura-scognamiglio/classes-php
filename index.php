7281fbe14a5a098bc9f2f5165fcf22a448e9a673
[laura-scognamiglio] <91824380+laura-scognamiglio@users.noreply.github.com>
2021-12-29 12:17:05 +0100

    disconnect done

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en" class="insHtml">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+</head>
+<body class ="insBody">
+    <main>
+        <section class= "formulaire">
+            <h2 class= "sous-titre" >Inscription</h2>
+            <form class= "form"  method= "post">
+               
+                    <div class= "form-group">
+                        <input type= "text" name= "login" placeholder= "login" autocomplete= "off">
+                    </div>
+                    <div class= "form-group">
+                        <input type= "password" name= "password" placeholder= "password" autocomplete= "off">
+                    </div>
+                    
+                    <div class="form-group">
+                        <button type="submit" name= "submit" class="btn ">Valider</button>
+                    </div> 
+                    <div class="form-group">
+                        <button type="submit" name= "logout" class="btn ">Deco</button>
+                    </div> 
+            </form>
+        </section>
+    </main>
+    
+</body>
+</html>
+
--- user.php
+++ user.php
@@ -1,5 +1,5 @@
 <?php
-
+include('index.php');
 //création d'une classe user avec ses attributs privés ou publiques
 class User{
     private $id;
@@ -63,35 +63,83 @@ class User{
     public function read(){
 
         $register = $this->con();
-
-        $this->read = mysqli_query($register, "SELECT * FROM `utilisateurs`");
+        $post_login = htmlspecialchars(trim($_POST['login']));
+        $this->read = mysqli_query($register, "SELECT * FROM `utilisateurs` WHERE `login`= '$post_login'");
         $read_fetch = mysqli_fetch_all($this->read, MYSQLI_ASSOC);
 
         foreach($read_fetch as $read){
-            echo $read["firstname"];
-            echo $read["lastname"];
-            echo $read["login"];
+            echo "<pre>";
+            echo $read["login"] . " " . $read["password"] ;
+            echo "</pre>";
         }
-    //    echo "<pre>";
-    //    var_dump($read_fetch);
-    //    echo "</pre>";
+   
 
-
-    }
-
-    public function connect($login, $password){
-        $this->login = $login;
-        $this->password = $password;
-
+    return($read_fetch);
 
     }
+
+    public function connect(){
+       
+       
+        $checking = $this->read();
+
+        // $arr_login = array_column($checking, "login");
+        // $arr_pwd = array_column($checking, "password" );
+        echo "<pre>";
+        var_dump($checking);
+        echo "</pre>";
+       
+        
+
+        if(isset($_POST["submit"]))
+        {
+        //   
+            
+            if(count($checking) != 0)
+            {
+                if($post_login  == $checking["login"]){
+                echo "user connected";
+                session_start();
+                    
+                }
+                
+            }else
+                {
+                    echo "the id is wrong ";
+                }
+            
     
+        }
+        
+
+    }
+
+
+    public function disconnect(){
+        $session_to_deco = $this->connect();
+
+        if(isset($_POST["logout"]))
+        {
+            session_destroy();
+           
+        }
+
+    }
+
+    public function delete(){
+
+        $delete = mysqli_query($register,);
+        $session_to_delete = $this->disconnect();
+
+    }
 }
+    
+
 
 
 $user = new User();
-$user->register("ioia","aa","aa","aa","aa");
+// $user->register("ioia","aa","aa","aa","aa");
 // $user->read();
-// $user2 = new User();
-// $user2->register("lili","kk","bb","bb","ii");
+$user->connect();
+
 
